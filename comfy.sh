33e1f73cea7e4a9aa0af3b373f2806a08c869c79
Mathias Kulhandjian <mathias.kulhandjian@gmail.com>
2025-07-10 16:32:52 +0200

    Create comfy.sh

---
+++ comfy.sh
@@ -0,0 +1,1 @@
+HOST=127.0.0.1 CMD='python3 /workspace/ComfyUI/main.py --disable-auto-launch --port 18188 --enable-cors-header' COMFYUI_PORT_HOST=18188 BASE='' COMFY_HOME=/workspace/ComfyUI ./comfyui-api

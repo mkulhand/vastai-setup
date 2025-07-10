18e42bc4cb38e8d28b3a790de1c5a98fab9b98b0
Mathias Kulhandjian <mathias.kulhandjian@gmail.com>
2025-07-10 17:05:34 +0200

    Update comfy.sh

--- comfy.sh
+++ comfy.sh
@@ -1,1 +1,4 @@
+#!/bin/bash
+
+source /venv/main/bin/activate
 HOST=127.0.0.1 CMD='python3 /workspace/ComfyUI/main.py --disable-auto-launch --port 18188 --enable-cors-header' COMFYUI_PORT_HOST=18188 BASE='' COMFY_HOME=/workspace/ComfyUI ./comfyui-api

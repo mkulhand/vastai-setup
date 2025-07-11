4494a114fbb2233c5285365145568e26f5dc9622
Mathias Kulhandjian <mathias.kulhandjian@gmail.com>
2025-07-11 14:15:20 +0200

    Update comfy.sh

--- comfy.sh
+++ comfy.sh
@@ -4,4 +4,4 @@
 sudo kill -9 $(sudo lsof -t -i :18188 -c python3)
 
 source /venv/main/bin/activate
-HOST=127.0.0.1 CMD='python3 /workspace/ComfyUI/main.py --disable-auto-launch --port 18188 --enable-cors-header' COMFYUI_PORT_HOST=18188 BASE='' COMFY_HOME=/workspace/ComfyUI ./comfyui-api
+OUTPUT_DIR="/workspace/outputs" HOST=127.0.0.1 CMD='python3 /workspace/ComfyUI/main.py --disable-auto-launch --port 18188 --enable-cors-header' COMFYUI_PORT_HOST=18188 BASE='' COMFY_HOME=/workspace/ComfyUI ./comfyui-api

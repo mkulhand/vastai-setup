f324552fe5269ab504092f8d3ed7e896de2438e9
Mathias Kulhandjian <mathias.kulhandjian@gmail.com>
2025-07-11 14:14:41 +0200

    Update comfy.sh

--- comfy.sh
+++ comfy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #kill any comfyUI process before starting comfy-api
-sudo kill -9 $(sudo lsof -t -i :18188)
+sudo kill -9 $(sudo lsof -t -i :18188 -c python3)
 
 source /venv/main/bin/activate
 HOST=127.0.0.1 CMD='python3 /workspace/ComfyUI/main.py --disable-auto-launch --port 18188 --enable-cors-header' COMFYUI_PORT_HOST=18188 BASE='' COMFY_HOME=/workspace/ComfyUI ./comfyui-api
